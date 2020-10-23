2d3d25e2e92c64c8a8a3864b66181db88d64ee6a
Himanshu Sajwan <57579061+HimanshuSajwan911@users.noreply.github.com>
2020-10-23 13:16:08 +0530

    Tic-Tac-Toe Game
    A two player tic-tac-toe game.

--- Mini_Projects/sem3/Tic-Tac-Toe.cpp
+++ Mini_Projects/sem3/Tic-Tac-Toe.cpp
@@ -28,101 +28,121 @@ int checkGameStatus(char grid[3][3],int CurrentPlayer){
 }
 
 int main(){
-game_start:
-    string playerX="",playerO="",FirstPlayer="";
-    int CurrentPlayer=0,winner=0;
-    int r=0,c=0,GameStaus=1;
-    char grid[3][3]={'.','.','.','.','.','.','.','.','.'};
-    cout<<"Enter a name for the X player: \n";
-    cin>>playerX;
-    cout<<"Enter a name for the O player: \n";
-    cin>>playerO;
 
+    //Main Loop.
     while(1){
-        cout<<"Who plays first, "<<playerX<<" or "<<playerO<<"?\n";
-        cin>>FirstPlayer;
-        if(FirstPlayer==playerX){
-            CurrentPlayer=1;
-            break;
-        }
-        else if(FirstPlayer==playerO){
-            CurrentPlayer=2;
-            break;
-        }
-        else{
-            cout<<FirstPlayer<<" is not a registered player.\n";
-        }
-    }
+        string playerX="",playerO="",FirstPlayer="";
+        int CurrentPlayer=0,winner=0;
+        int r=0,c=0,GameStaus=1,Play=0;
+        char grid[3][3]={'.','.','.','.','.','.','.','.','.'};
+        cout<<"Enter a name for the X player: \n";
+        cin>>playerX;
+        cout<<"Enter a name for the O player: \n";
+        cin>>playerO;
 
-    while(1){
-        showGrid(grid);
-        if(CurrentPlayer==1){
-            cout<<"Player of current turn: "<<playerX<<"\n";
-        }
-        else{
-            cout<<"Player of current turn: "<<playerO<<"\n";
-        }
-        Checkpoint_vald_Row_Column:
-            Checkpoint:
+        //First Turn Loop.
+        while(1){
+            cout<<"Who plays first, "<<playerX<<" or "<<playerO<<"?\n";
+            cin>>FirstPlayer;
+            if(FirstPlayer==playerX){
+                CurrentPlayer=1;
+                break;
+            }
+            else if(FirstPlayer==playerO){
+                CurrentPlayer=2;
+                break;
+            }
+            else{
+                cout<<FirstPlayer<<" is not a registered player.\n";
+            }
+        }//End of First Turn Loop.
+
+        //Game Loop.
+        while(1){
+            showGrid(grid);
+            if(CurrentPlayer==1){
+                cout<<"Player of current turn: "<<playerX<<"\n";
+            }
+            else{
+                cout<<"Player of current turn: "<<playerO<<"\n";
+            }
+
+            //Valid Cell Loop.
+            while(1){
                 cout<<"Choose a row number (0 to 2):\n";
                 cin>>r;
                 if(r<0||r>2){
                     cout<<r<<" is not a valid row.\n";
-                    goto Checkpoint;
+                    continue;
                 }
 
                 cout<<"Choose a column number (0 to 2):\n";
                 cin>>c;
                 if(c<0||c>2){
                     cout<<c<<" is not a valid column.\n";
-                    goto Checkpoint;
+                    continue;
                 }
-            if(grid[r][c]=='.'){
-                if(CurrentPlayer==1){
-                    grid[r][c]='X';
-                    CurrentPlayer=2;
+
+                if(grid[r][c]=='.'){
+                    if(CurrentPlayer==1){
+                        grid[r][c]='X';
+                        CurrentPlayer=2;
+                    }
+                    else{
+                        grid[r][c]='O';
+                        CurrentPlayer=1;
+                    }
+                    break;
                 }
                 else{
-                    grid[r][c]='O';
-                    CurrentPlayer=1;
+                    cout<<"Slot is already taken.\n";
+                    continue;
                 }
+            }//End of Valid Cell Loop.
+
+            winner=checkGameStatus(grid,CurrentPlayer);
+            if(winner==1){
+                cout<<"\nGame is over: \n"<<playerO<<" wins!\n";
+                CurrentPlayer=0;
+                break;
+            }
+            else if(winner==2){
+                cout<<"\nGame is over: \n"<<playerX<<" wins!\n";
+                CurrentPlayer=0;
+                break;
+            }
+            if(GameStaus++>=9){
+                cout<<"Game is over: \nit is a tie!";
+                break;
+            }
+        }//End of Game Loop.
+
+        showGrid(grid);
+
+        //Replay Loop.
+        while(1){
+            string again="";
+            cout<<"\nWould you like to play again? (Y/N)\n";
+            cin>>again;
+            if(again=="y"||again=="Y"){
+                Play=1;
+                break;
+            }
+            else if(again=="n"||again=="N"){
+                cout<<"Bye!";
+                Play=0;
+                break;
             }
             else{
-                cout<<"Slot is already taken.\n";
-                goto Checkpoint_vald_Row_Column;
+                cout<<again<<" is not a valid answer.";
             }
-        winner=checkGameStatus(grid,CurrentPlayer);
-        if(winner==1){
-            cout<<"Game is over: \n"<<playerO<<" wins!\n";
-            CurrentPlayer=0;
+        }//End of Replay Loop.
+
+        if(!Play){
             break;
         }
-        else if(winner==2){
-            cout<<"Game is over: \n"<<playerX<<" wins!\n";
-            CurrentPlayer=0;
-            break;
-        }
-        if(GameStaus++>=9){
-            cout<<"Game is over: \nit is a tie!";
-            break;
-        }
-    }
-    showGrid(grid);
-    while(1){
-        string again="";
-        cout<<"\nWould you like to play again? (Y/N)\n";
-        cin>>again;
-        if(again=="y"||again=="Y"){
-            goto game_start;
-        }
-        else if(again=="n"||again=="N"){
-            cout<<"Bye!";
-            break;
-        }
-        else{
-            cout<<again<<" is not a valid answer.";
-        }
-    }
+
+    }//End of Main Loop.
 
     return 0;
 }
